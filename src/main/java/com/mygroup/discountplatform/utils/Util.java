94116dde116213ac41053a0de24df882bfe04429
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2026-01-17 14:27:59 -0300

    adding function to calculate time ago as human‑readable string

--- src/main/java/com/mygroup/discountplatform/utils/Util.java
+++ src/main/java/com/mygroup/discountplatform/utils/Util.java
@@ -2,6 +2,10 @@ package com.mygroup.discountplatform.utils;
 
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Period;
+
 @Component
 public class Util {
 
@@ -50,5 +54,39 @@ public class Util {
         if (dv == 11) return '0';
         else if (dv == 10) return 'K';
         else return (char) (dv + '0');
+    }
+
+    /**
+     * Returns time ago as human‑readable string
+     */
+    public String formatTimeAgo(LocalDateTime past){
+        LocalDateTime now = LocalDateTime.now();
+        Duration duration = Duration.between(past, now);
+
+        // Returns human‑readable time difference as string
+        if (duration.toDays() > 0) {
+            Period period = Period.between(past.toLocalDate(), now.toLocalDate());
+
+            // Returns years, months, or days as human‑readable string
+            if (period.getYears() > 0) {
+                int years = period.getYears();
+                return years == 1 ? "One year ago" : years + " years ago";
+            } else if (period.getMonths() > 0) {
+                int months = period.getMonths();
+                return months == 1 ? "One month ago" : months + " months ago";
+            } else {
+                int days = period.getDays();
+                return days == 1 ? "One day ago" : days + " days ago";
+            }
+        } else if (duration.toHours() > 0) {
+            long hours = duration.toHours();
+            return hours == 1 ? "One hour ago" : hours + " hours ago";
+        } else if (duration.toMinutes() > 0) {
+            long minutes = duration.toMinutes();
+            return minutes == 1 ? "One minute ago" : minutes + " minutes ago";
+        } else {
+            long seconds = duration.getSeconds();
+            return seconds == 1 ? "One second ago" : seconds + " seconds ago";
+        }
     }
 }
